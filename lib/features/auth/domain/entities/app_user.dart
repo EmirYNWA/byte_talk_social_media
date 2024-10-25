c339176684679b8aa9ec76a784dcc74ad22d504e
KristinaJosipovic <kristina.josipovic21@gmail.com>
2024-10-25 18:38:16 +0200

    Added logo. Created initial folder structure for authentication and added app user class.

---
+++ lib/features/auth/domain/entities/app_user.dart
@@ -0,0 +1,39 @@
+class AppUser {
+  final String uid;
+  final String username;
+  final String name;
+  final String lastname;
+  final String email;
+  final String password;
+
+  AppUser({
+    required this.uid,
+    required this.username,
+    required this.name,
+    required this.lastname,
+    required this.email,
+    required this.password,
+
+  });
+
+  Map<String, dynamic> toJson() {
+    return{
+      'uid': uid,
+      'username': username,
+      'name': name,
+      'lastname': lastname,
+      'email': email,
+      'password': password,
+    };
+  }
+  factory AppUser.fromJson(Map<String, dynamic> jsonUser){
+    return AppUser(
+        uid: jsonUser ['uid'],
+        username: jsonUser ['username'],
+        name: jsonUser ['name'],
+        lastname: jsonUser ['lastname'],
+        email: jsonUser ['email'],
+        password: jsonUser ['password'],
+    );
+  }
+}
