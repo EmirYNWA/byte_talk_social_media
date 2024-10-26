1899cc41cc89376616598674ee8b523e7504431c
edvinbakalovic <edvin.bakalovic04@gmail.com>
2024-10-26 12:58:17 +0200

    Edit_profile_page fixed

--- lib/features/profile/presentation/pages/edit_profile_page.dart
+++ lib/features/profile/presentation/pages/edit_profile_page.dart
@@ -1,9 +1,13 @@
+import 'dart:io';
+
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:social_media_app/features/profile/domain/entities/profile_user.dart';
 import 'package:social_media_app/features/profile/presentation/cubits/profile_cubit.dart';
-
+import 'package:flutter/foundation.dart' show Uint8List, kIsWeb;
 import '../../../auth/presentaion/components/my_text_field.dart';
 import '../cubits/profile_states.dart';
 
@@ -19,19 +23,57 @@ class EditProfilePage extends StatefulWidget {
 }
 
 class _EditProfilePageState extends State<EditProfilePage> {
+  // mobile image pick
+  PlatformFile? imagePickedFile;
 
+  // web image pick
+  Uint8List? webImage;
+
+  // bio text controller
   final bioTextController = TextEditingController();
+
+  // pick image
+  Future<void> pickImage()async{
+    final result = await FilePicker.platform.pickFiles(
+      type: FileType.image,
+      withData: kIsWeb,
+    );
+
+    if(result != null){
+      setState(() {
+        imagePickedFile = result.files.first;
+
+        if(kIsWeb){
+          webImage = imagePickedFile!.bytes;
+        }
+      });
+    }
+  }
 
   // update profile button pressed
   void updateProfile()async{
     // profile cubit
     final profileCubit = context.read<ProfileCubit>();
 
-    if(bioTextController.text.isNotEmpty){
+    // prepare images & data
+    final String uid = widget.user.uid;
+    final String? newBio =
+    bioTextController.text.isNotEmpty ? bioTextController.text : null;
+    final imageMobilePath = kIsWeb ? null : imagePickedFile?.path;
+    final imageWebBytes = kIsWeb ? imagePickedFile?.bytes : null;
+
+
+
+    // only update profile if there is something to update
+    if(imagePickedFile != null || newBio != null ){
       profileCubit.updateProfile(
         uid: widget.user.uid,
         newBio: bioTextController.text,
       );
+    }
+    // nothing to update -> go to previous page
+    else{
+      Navigator.pop(context);
     }
   }
 
@@ -70,7 +112,7 @@ class _EditProfilePageState extends State<EditProfilePage> {
     );
   }
 
-  Widget buildEditPage({double uploadProgress = 0.0}) {
+  Widget buildEditPage() {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Edit Profile"),
@@ -87,7 +129,53 @@ class _EditProfilePageState extends State<EditProfilePage> {
       body: Column(
         children: [
           // profile picture
+            Center(
+              child: Container(
+                height: 200,
+                width: 200,
+                decoration: BoxDecoration(
+                  color:  Theme.of(context).colorScheme.secondary,
+                  shape: BoxShape.circle,
+                ),
 
+                child:
+                // display selected image for mobile
+                (!kIsWeb && imagePickedFile != null)
+                ? Image.file(
+                    File(imagePickedFile!.path!),
+                  )
+                :
+                // display selected image for web
+                (kIsWeb && webImage != null)
+                 ? Image.memory(webImage!)
+                 :
+                // no image selected -> display existing profile pic
+                CachedNetworkImage(
+                  imageUrl: widget.user.profileImageUrl,
+                  // loading..
+                  placeholder:(context, url) =>
+                    const CircularProgressIndicator(),
+                  //error -> failed to load
+                  errorWidget: (context,url,error)=> Icon(
+                    Icons.person,
+                    size: 72,
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                  //loaded
+                  imageBuilder: (context, imageProvider)=>
+                      Image(image: imageProvider),
+                ),
+              ),
+            ),
+          const SizedBox(height: 25),
+          // pick image button
+           Center(
+             child: MaterialButton(
+                 onPressed: pickImage,
+                 color: Colors.blue,
+                 child:const Text("Pick Image"),
+             ),
+           ),
           //bio
           Text("Bio"),
 
