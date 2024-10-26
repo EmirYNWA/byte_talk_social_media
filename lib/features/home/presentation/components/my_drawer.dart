5df1123ac7a0e3191509e1ed6f61ebe9531b87c5
KristinaJosipovic <kristina.josipovic21@gmail.com>
2024-10-26 15:16:16 +0200

    Logout button

--- lib/features/home/presentation/components/my_drawer.dart
+++ lib/features/home/presentation/components/my_drawer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:social_media_app/features/post/presentation/pages/upload_post_page.dart';
 import '../../../auth/presentaion/cubits/auth_cubit.dart';
 import '../../../profile/presentation/pages/profile_page.dart';
 import 'my_drawer_tile.dart';
@@ -64,6 +65,19 @@ class MyDrawer extends StatelessWidget{
                   onTap: () {},
                 ),
 
+                MyDrawerTile(
+                  title: "A D D  P I C T U R E",
+                  icon: Icons.add,
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder:(context)=>UploadPostPage(),
+                      ),
+                    );
+                  },
+                ),
+
                 // settings tile
                 MyDrawerTile(
                   title: "S E T T I N G S",
@@ -77,7 +91,8 @@ class MyDrawer extends StatelessWidget{
                 MyDrawerTile(
                   title: "L O G O U T",
                   icon: Icons.login,
-                  onTap: () => context.read<AuthCubit>().logout(),
+                  onTap: () {context.read<AuthCubit>().logout();
+                  Navigator.of(context).pushReplacementNamed('/login');},
                 ),
               ],
             ),
