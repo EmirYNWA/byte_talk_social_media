8fe39674640bd2c150ffaad608c67572799f493a
Emir Halilović <66942813+EmirYNWA@users.noreply.github.com>
2024-10-26 13:19:03 +0200

    Developed core functionalities for messaging between users including messaging services and connection to DB.

---
+++ lib/features/chat/data/chat_service.dart
@@ -0,0 +1,31 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class ChatService {
+  // get instance of firestore
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // get user stream
+  Stream<List<Map<String, dynamic>>> getUsersStream() {
+    return _firestore.collection("users").snapshots().map((snapshot) {
+      return snapshot.docs.map((doc) {
+        final user = doc.data();
+        return user;
+      }).toList();
+    });
+  }
+
+  // send message
+  Future<void> sendMessage(String receiverId, message) async {
+    // get current user info
+    final String currentUserID = _auth.currentUser!.uid;
+    final String currentUserEmail = _auth.currentUser!.email!;
+    final Timestamp timestamp = Timestamp.now();
+    // create new chat
+
+    // construct chat room ID
+  }
+}
+
+
