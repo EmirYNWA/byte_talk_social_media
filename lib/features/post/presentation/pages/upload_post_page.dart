08050ae255cbfc1002211c3f003523b21eec2905
KristinaJosipovic <kristina.josipovic21@gmail.com>
2024-10-26 11:32:07 +0200

    Upload page upgraded.

--- lib/features/post/presentation/pages/upload_post_page.dart
+++ lib/features/post/presentation/pages/upload_post_page.dart
@@ -1,9 +1,13 @@
 import 'dart:typed_data';
 
 import 'package:file_picker/file_picker.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:social_media_app/features/auth/presentaion/cubits/auth_cubit.dart';
+import 'package:social_media_app/features/post/domain/entities/post.dart';
+import 'package:social_media_app/features/post/presentation/cubits/post_cubits.dart';
+import 'package:social_media_app/features/post/presentation/cubits/post_states.dart';
 import '../../../auth/domain/entities/app_user.dart';
 
 class UploadPostPage extends StatefulWidget {
@@ -32,14 +36,44 @@ class _UploadPostPageState extends State<UploadPostPage> {
     final authCubit = context.read<AuthCubit>();
     currentUser = authCubit.currentUser;
   }
+  //pickImage void
+
+  void uploadPost(){
+    if (imagePickedFile == null || textController.text.isEmpty){
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("Both image and caption are required")));
+      return;
+    }
+    final newPost = Post(
+        id: DateTime.now().microsecondsSinceEpoch.toString(),
+        userId: currentUser!.uid,
+        username: currentUser!.name,
+        text: textController.text,
+        imageUrl: '',
+        timestamp: DateTime.now());
+    final postCubit = context.read<PostCubit>();
+    if (kIsWeb){
+      postCubit.createPost(newPost,imageBytes: imagePickedFile?.bytes);
+    }else{
+      postCubit.createPost(newPost, imagePath: imagePickedFile?.path);
+    }
+  }
+
+  @override
+  void dispose() {
+    textController.dispose();
+    super.dispose();
+  }
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Create Post'),
-        foregroundColor: Theme.of(context).colorScheme.primary,
-      ),
-
-
-    );
+    return BlocConsumer(
+        builder: (context, state){
+          if (state is PostsLoading || state is PostsUpLoading) {
+            return const Scaffold(
+              body: Center(child: CircularProgressIndicator(),),
+            );
+          }
+          return buildUploadPage();
+        },
+        listener: listener)
   }
 }
